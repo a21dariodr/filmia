eef0645bf4c9bd10784b1de310c984ab45e12b2c
a21dariodr <a21dariodr@iessanclemente.net>
2023-10-22 23:23:53 +0200

    Typescript, Atropos y Animate.css añadidos al proyecto

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,4 +19,3 @@ module.exports = {
     'react/prop-types': false,
   },
 }
-
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { Button } from '@material-tailwind/react'
 
-function App() {
-
+const App = () => {
   console.log(import.meta.env.VITE_TMD_ACCESS_TOKEN)
 
   return (
-    <Button>Button</Button>
+    <>
+      <h1>Filmia</h1>
+      <Button>Button</Button>
+    </>
   )
 }
 
