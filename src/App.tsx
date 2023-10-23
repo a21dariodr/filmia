353ea81ccb6c71c68c461507275ee3a56d02580e
a21dariodr <a21dariodr@iessanclemente.net>
2023-10-23 09:23:02 +0200

    Firebase + hosting implementados

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import './App.css'
 import { Button } from '@material-tailwind/react'
+import { initializeApp } from 'firebase/app'
 
 const App = () => {
-  console.log(import.meta.env.VITE_TMD_ACCESS_TOKEN)
+  // Configuración para inicializar Firebase
+  const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_FIREBASE_APPID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID,
+  }
 
+  console.log('App iniciada')
+
+  const firebaseApp = initializeApp(firebaseConfig)
+  const tmdApiKey = import.meta.env.VITE_TMD_ACCESS_TOKEN
+  
   return (
     <>
       <h1>Filmia</h1>
       <Button>Button</Button>
+      <h2>Prueba de despliegue automático con pipeline</h2>
     </>
   )
 }
