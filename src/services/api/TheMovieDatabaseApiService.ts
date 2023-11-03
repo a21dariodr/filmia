5e82ed2401dcc7cb1d140043bec93de253da518d
a21dariodr <a21dariodr@iessanclemente.net>
2023-11-03 18:02:45 +0100

    Añadida configuración para API TMD

--- src/services/api/TheMovieDatabaseApiService.ts
+++ src/services/api/TheMovieDatabaseApiService.ts
@@ -3,11 +3,19 @@ import axios from 'axios'
 //TODO implementar métodos del servicio
 // Servicio para la obtención de datos de películas de la API The Movie Database
 export default class TheMovieDatabaseApiService {
-	
-    tmdApiKey = import.meta.env.VITE_TMD_ACCESS_TOKEN
+    readonly tmdApiKey = import.meta.env.VITE_TMD_ACCESS_TOKEN
+
+    options = {
+        method: 'GET',
+        url: 'https://api.themoviedb.org/3/movie/575264',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer '+this.tmdApiKey
+        }
+    }
 
     public getMovieInfo() {
         //TODO implementar método
-        if (!axios) console.log("Llamada a la API de TMD")
+        if (!axios) console.log('Llamada a la API de TMD')
     }
 }
