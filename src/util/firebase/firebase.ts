3842e3288563ae0efdd09617c322b86f8b4c0787
a21dariodr <a21dariodr@iessanclemente.net>
2023-11-03 12:57:40 +0100

    Modificada configuración de Firebase

--- src/util/firebase/firebase.ts
+++ src/util/firebase/firebase.ts
@@ -3,25 +3,15 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore/lite'
 
 // Configuración para inicializar Firebase
-const firebaseConfig = import.meta.env.PROD
-    ? {
-          apiKey: 'import.meta.env.VITE_FIREBASE_APIKEY',
-          authDomain: 'import.meta.env.VITE_FIREBASE_AUTHDOMAIN',
-          projectId: 'import.meta.env.VITE_FIREBASE_PROJECTID',
-          storageBucket: 'import.meta.env.VITE_FIREBASE_STORAGEBUCKET',
-          messagingSenderId: 'import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID',
-          appId: 'import.meta.env.VITE_FIREBASE_APPID',
-          measurementId: 'import.meta.env.VITE_FIREBASE_MEASUREMENTID'
-      }
-    : {
-          apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-          authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
-          projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
-          storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
-          messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
-          appId: import.meta.env.VITE_FIREBASE_APPID,
-          measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID
-      }
+const firebaseConfig = {
+    apiKey: 'AIzaSyD8_oZeglhJbK5vdSJFV7Gq3EgVYbxk6aQ',
+    authDomain: 'filmia-ee910.firebaseapp.com',
+    projectId: 'filmia-ee910',
+    storageBucket: 'filmia - ee910.appspot.com',
+    messagingSenderId: '871935945294',
+    appId: '1:871935945294:web:d4f46e4993f12ac170311f',
+    measurementId: 'G-XTYQG689SB'
+}
 
 console.log(firebaseConfig)
 
