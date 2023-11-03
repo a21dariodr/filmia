93996c43bac7e684154cbd4d966865f5afe3f684
a21dariodr <a21dariodr@iessanclemente.net>
2023-11-03 12:19:17 +0100

    Modificación de importación de variables de entorno

--- src/util/firebase/firebase.ts
+++ src/util/firebase/firebase.ts
@@ -1,29 +1,27 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore/lite'
-import Login from '../../layouts/Login'
 
 // Configuración para inicializar Firebase
-const firebaseConfig =
-    import.meta.env.MODE === 'development'
-        ? {
-              apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-              authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
-              projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
-              storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
-              messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
-              appId: import.meta.env.VITE_FIREBASE_APPID,
-              measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID
-          }
-        : {
-              apiKey: 'import.meta.env.VITE_FIREBASE_APIKEY',
-              authDomain: 'import.meta.env.VITE_FIREBASE_AUTHDOMAIN',
-              projectId: 'import.meta.env.VITE_FIREBASE_PROJECTID',
-              storageBucket: 'import.meta.env.VITE_FIREBASE_STORAGEBUCKET',
-              messagingSenderId: 'import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID',
-              appId: 'import.meta.env.VITE_FIREBASE_APPID',
-              measurementId: 'import.meta.env.VITE_FIREBASE_MEASUREMENTID'
-          }
+const firebaseConfig = import.meta.env.PROD
+    ? {
+          apiKey: 'import.meta.env.VITE_FIREBASE_APIKEY',
+          authDomain: 'import.meta.env.VITE_FIREBASE_AUTHDOMAIN',
+          projectId: 'import.meta.env.VITE_FIREBASE_PROJECTID',
+          storageBucket: 'import.meta.env.VITE_FIREBASE_STORAGEBUCKET',
+          messagingSenderId: 'import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID',
+          appId: 'import.meta.env.VITE_FIREBASE_APPID',
+          measurementId: 'import.meta.env.VITE_FIREBASE_MEASUREMENTID'
+      }
+    : {
+          apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+          authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+          projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+          storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+          messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+          appId: import.meta.env.VITE_FIREBASE_APPID,
+          measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID
+      }
 
 console.log(firebaseConfig)
 
