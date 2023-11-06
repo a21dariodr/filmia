e1e9176bc87b64e5fd33cb4349fd58bcc73e0000
a21dariodr <a21dariodr@iessanclemente.net>
2023-11-06 23:39:48 +0100

    Añadido componente de selección de idioma

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,11 +16,11 @@ const Header = () => {
         <div className="w-100">
             <Navbar className="sticky top-0 z-10 max-w-full rounded-none p-2 lg:p-4 mb-2 lg:mb-4">
                 <div className="flex items-center justify-between text-blue-gray-900">
-                    <Link to={'/'} className="flex items-center gap-4 cursor-pointer font-bold">
-                        <img src="movie_clapperboard.svg" className="w-[12%]" alt="Filmia logo" />
-                        <span className="text-3xl">FILMIA</span>
+                    <Link to={'/'} className="flex items-center gap-2 md:gap-4 cursor-pointer font-bold">
+                        <img src="movie_clapperboard.svg" className="w-[25%] md:w-[12%]" alt="Filmia logo" />
+                        <span className="text-xl md:text-3xl">FILMIA</span>
                     </Link>
-                    <div className="flex gap-5">
+                    <div className="flex items-center md:items-stretch gap-5">
                         <LanguageSelect />
                         <Button onClick={closeSession} size="sm" className="text-xs bg-violet-700 hover:bg-violet-600">
                             {t('login.log_out_1_upper')}&nbsp;
--- src/components/LanguageSelect.tsx
+++ src/components/LanguageSelect.tsx
@@ -1,15 +1,44 @@
+import { useState, cloneElement } from 'react'
 import { Select, Option } from '@material-tailwind/react'
 import { useTranslation } from 'react-i18next'
+import '../styles/LanguageSelect.css'
 
 const LanguageSelect = () => {
-	const { t } = useTranslation()
+    const { t, i18n } = useTranslation()
+	const [ selectedLanguage, setSelectedLanguage ] = useState<string>(i18n.resolvedLanguage ? i18n.resolvedLanguage : 'es')
 
-	return (
-        <div>
-            <Select label={t('languages.select')}>
-                <Option value="spanish">{t('languages.spanish')}</Option>
-                <Option value="galician">{t('languages.galician')}</Option>
-                <Option value="english">{t('languages.english')}</Option>
+	const languageChangeHandler = (e: any) => {
+		i18n.changeLanguage(e)
+		setSelectedLanguage(e)
+	}
+
+    return (
+        <div id='languageSelect'>
+            <Select
+                label={t('languages.select')}
+                size="lg"
+                color="deep-purple"
+                selected={element =>
+                    element &&
+                    cloneElement(element, {
+                        disabled: true,
+                        className: 'flex items-center opacity-100 px-0 gap-2 pointer-events-none'
+                    })
+                }
+                value={selectedLanguage}
+                onChange={languageChangeHandler}>
+                <Option className="flex items-center gap-2" value={'es'}>
+                    <img className="h-5 w-5 rounded-full object-cover" src="flags/es.svg" alt="Español" />
+                    {t('languages.spanish')}
+                </Option>
+                <Option className="flex items-center gap-2" value={'gl'}>
+                    <img className="h-5 w-5 rounded-full object-cover" src="flags/es-ga.svg" alt="Español" />
+                    {t('languages.galician')}
+                </Option>
+                <Option className="flex items-center gap-2" value={'en'}>
+                    <img className="h-5 w-5 rounded-full object-cover" src="flags/gb.svg" alt="Español" />
+                    {t('languages.english')}
+                </Option>
             </Select>
         </div>
     )
