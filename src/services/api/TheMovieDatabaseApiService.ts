109c10861b6381a7d97f134bc246d251d0731c65
a21dariodr <a21dariodr@iessanclemente.net>
2023-11-07 22:19:07 +0100

    Cambios menores en el servicio de llamadas a la API TMDB

--- src/services/api/TheMovieDatabaseApiService.ts
+++ src/services/api/TheMovieDatabaseApiService.ts
@@ -3,19 +3,36 @@ import axios from 'axios'
 //TODO implementar métodos del servicio
 // Servicio para la obtención de datos de películas de la API The Movie Database
 export default class TheMovieDatabaseApiService {
-    readonly tmdApiKey = import.meta.env.VITE_TMD_ACCESS_TOKEN
+    readonly tmdApiAccessToken = import.meta.env.VITE_TMD_ACCESS_TOKEN
+    readonly baseUrl = 'https://api.themoviedb.org/3/'
+    readonly imageBaseUrl = 'http://image.tmdb.org/t/p/'
+    readonly appendToResponse = '&append_to_response='
 
     options = {
         method: 'GET',
-        url: 'https://api.themoviedb.org/3/',
+        url: '',
+        params: {
+            query: '',
+            include_adult: 'true',
+            language: 'es',
+            page: '1'
+        },
         headers: {
             accept: 'application/json',
-            Authorization: 'Bearer '+this.tmdApiKey
+            Authorization: 'Bearer ' + this.tmdApiAccessToken
         }
     }
 
-    public getMovieInfo() {
+    public getMovieById() {
         //TODO implementar método
         if (!axios) console.log('Llamada a la API de TMD')
+    }
+
+	//TODO añadir procesamiento para crear objetos Film con la info y devolverlos en un array
+    public searchMovieByTitle(titleQuery: string) {
+        let options = { ...this.options }
+        options.url = this.baseUrl + '/search/movie'
+		options.params.query = titleQuery
+		return axios.request(options)
     }
 }
