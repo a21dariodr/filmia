b96c6fdc3f7fa15e974325fa8007fdab09ffbe47
a21dariodr <a21dariodr@iessanclemente.net>
2023-11-08 22:47:52 +0100

    Implementado servicio para empleo de la API de TMDB

---
+++ src/models/Film.tsx
@@ -0,0 +1,148 @@
+export type ProductionCompany = {
+	name: string
+	logoUrl: string
+	originCountry: string
+}
+
+export type ProductionCountry = {
+    name: string
+    iso3166: string
+}
+
+/* Clase para mapear los resultados de consultas a la API de TMDB, que puede ser muy útil para un posible
+ * guardado de películas en Firestore
+ */
+export class Film {
+    private _id: number
+    private _title: string
+    private _originalTitle: string
+    private _originalLanguage?: string
+    private _duration?: number
+    private _releaseYear: string
+    private _genres?: string[]
+    private _overview?: string
+    private _posterPath: string
+    private _productionCountries?: ProductionCountry[]
+    private _productionCompanies?: ProductionCompany[]
+    private _voteAverage: number
+    private _revenue?: number
+    private _popularity?: number
+    private _tagLine?: string
+
+    /* Constructor con parámetros obligatorios usados en la búsqueda de películas
+     * Los demás parámetros de emplean a través de los getters y setters
+     */
+    constructor(id: number, title: string, originalTitle: string, releseaseDate: string, posterPath: string, voteAverage: number) {
+        this._id = id
+        this._title = title
+        this._originalTitle = originalTitle
+        this._releaseYear = releseaseDate.substring(0, 4)
+        this._posterPath = posterPath
+        this._voteAverage = voteAverage
+    }
+
+    public get id(): number {
+        return this._id
+    }
+    public set id(value: number) {
+        this._id = value
+    }
+
+    public get title(): string {
+        return this._title
+    }
+    public set title(value: string) {
+        this._title = value
+    }
+
+    public get originalTitle(): string {
+        return this._originalTitle
+    }
+    public set originalTitle(value: string) {
+        this._originalTitle = value
+    }
+
+    public get originalLanguage(): string | undefined {
+        return this._originalLanguage
+    }
+    public set originalLanguage(value: string | undefined) {
+        this._originalLanguage = value
+    }
+
+    public get duration(): number | undefined {
+        return this._duration
+    }
+    public set duration(value: number | undefined) {
+        this._duration = value
+    }
+
+    public get releaseDate(): string {
+        return this._releaseYear
+    }
+    public set releaseDate(value: string) {
+        this._releaseYear = value
+    }
+
+    public get genres(): string[] | undefined {
+        return this._genres
+    }
+    public set genres(value: string[] | undefined) {
+        this._genres = value
+    }
+
+    public get overview(): string | undefined {
+        return this._overview
+    }
+    public set overview(value: string | undefined) {
+        this._overview = value
+    }
+
+    public get posterPath(): string {
+        return this._posterPath
+    }
+    public set posterPath(value: string) {
+        this._posterPath = value
+    }
+
+    public get productionCountries(): ProductionCountry[] | undefined {
+        return this._productionCountries
+    }
+    public set productionCountries(value: ProductionCountry[] | undefined) {
+        this._productionCountries = value
+    }
+
+    public get productionCompanies(): ProductionCompany[] | undefined {
+        return this._productionCompanies
+    }
+    public set productionCompanies(value: ProductionCompany[] | undefined) {
+        this._productionCompanies = value
+    }
+
+    public get voteAverage(): number {
+        return this._voteAverage
+    }
+    public set voteAverage(value: number) {
+        this._voteAverage = value
+    }
+
+    public get revenue(): number | undefined {
+        return this._revenue
+    }
+    public set revenue(value: number | undefined) {
+        this._revenue = value
+    }
+
+    public get popularity(): number | undefined {
+        return this._popularity
+    }
+    public set popularity(value: number | undefined) {
+        this._popularity = value
+    }
+
+    public get tagLine(): string | undefined {
+        return this._tagLine
+    }
+    public set tagLine(value: string | undefined) {
+        this._tagLine = value
+    }
+}
--- src/services/api/TheMovieDatabaseApiService.ts
+++ src/services/api/TheMovieDatabaseApiService.ts
@@ -1,21 +1,21 @@
 import axios from 'axios'
+import { Film, ProductionCompany, ProductionCountry } from '../../models/Film';
 
-//TODO implementar métodos del servicio
 // Servicio para la obtención de datos de películas de la API The Movie Database
 export default class TheMovieDatabaseApiService {
     readonly tmdApiAccessToken = import.meta.env.VITE_TMD_ACCESS_TOKEN
     readonly baseUrl = 'https://api.themoviedb.org/3/'
     readonly imageBaseUrl = 'https://image.tmdb.org/t/p/'
     readonly appendToResponse = '&append_to_response='
-    readonly imageSizes = ['w92/', 'w154/', 'w185/', 'w342/', 'w500/', 'w780/', 'original/']
+    readonly imageSizes = ['w92', 'w154', 'w185', 'w342', 'w500', 'w780', 'original']
 
-    options = {
+    private options = {
         method: 'GET',
         url: '',
         params: {
             query: '',
-            include_adult: 'true',
-            language: 'es',
+            include_adult: '',
+            language: 'es-ES',
             page: ''
         },
         headers: {
@@ -24,30 +24,79 @@ export default class TheMovieDatabaseApiService {
         }
     }
 
-    //TODO añadir procesamiento paracrear objeto film con la info y devolverlo
-	//TODO implementar funcion mapper?
     public async getMovieById(id: string) {
-		let options = { ...this.options }
-		options.url = this.baseUrl + 'movie/' + id
+        let options = { ...this.options }
+        options.url = this.baseUrl + 'movie/' + id
 
-		const filmInfo = await axios.request(options)
-		const film = {}
+        const filmInfo: any = await axios.request(options)
+        const film: Film = this.movieGetByIdMapper(filmInfo)
 
-		return film
+        return film
     }
 
-    //TODO añadir procesamiento para crear objetos Film con la info y devolverlos en un array
-    //TODO añadir url completa de la imagen al objeto
-	//TODO implementar función mapper?
     public async searchMovieByTitle(titleQuery: string) {
         let options = { ...this.options }
         options.url = this.baseUrl + 'search/movie'
         options.params.query = titleQuery
-		options.params.page = '1'
+        options.params.page = '1'
+        options.params.include_adult = 'true'
 
-        const searchResult = await axios.request(options)
-		const filmsFound: any = []
+        const response: any = await axios.request(options)
+        const searchResult = response.results
+        const filmsFound: Film[] = this.movieSearchByTitleMapper(searchResult)
 
-		return filmsFound
+        return filmsFound
     }
+
+    private movieGetByIdMapper(filmInfo: any): Film {
+        const posterUrl = this.baseUrl + this.imageSizes[4] + filmInfo.poster_path
+
+        const film: Film = new Film(filmInfo.id, filmInfo.title, filmInfo.original_title, filmInfo.release_date, posterUrl, filmInfo.vote_average)
+
+        film.originalLanguage = filmInfo.original_language
+        film.duration = filmInfo.runtime
+        film.genres = this.filmGenresMapper(filmInfo.genres)
+        film.overview = filmInfo.overview
+        film.productionCountries = this.filmProductionCountriesMapper(filmInfo.production_countries)
+		film.productionCompanies = this.filmProductionCompaniesMapper(filmInfo.production_companies)
+		film.revenue = filmInfo.revenue
+        film.popularity = filmInfo.popularity
+        film.tagLine = filmInfo.tagline
+
+        return film
+    }
+
+    private movieSearchByTitleMapper(searchResult: any): Film[] {
+        const filmsFound: Film[] = []
+
+        searchResult.forEach((filmResult: any) => {
+            const posterUrl = this.baseUrl + this.imageSizes[2] + filmResult.poster_path
+            const film = new Film(searchResult.id, searchResult.title, searchResult.original_title, searchResult.release_date, posterUrl, searchResult.vote_average)
+            filmsFound.push(film)
+        })
+
+        return filmsFound
+    }
+
+    private filmGenresMapper(genresResult: any): string[] {
+        const genres: string[] = genresResult.map((genre: any) => genre.name)
+        return genres
+    }
+
+    private filmProductionCountriesMapper(productionCountriesResult: any): ProductionCountry[] {
+		const productionCountries: ProductionCountry[] = productionCountriesResult.map( (productionCountry: any): ProductionCountry => {
+			return { name: productionCountry.name, iso3166: productionCountry.iso_3166_1 }
+		} )
+		return productionCountries
+	}
+
+    private filmProductionCompaniesMapper(productionCompaniesResult: any): ProductionCompany[] {
+		const productionCompanies: ProductionCompany[] = productionCompaniesResult.map( (productionCompany: any): ProductionCompany => {
+			return { name: productionCompany.name,
+				originCountry: productionCompany.origin_country,
+				logoUrl: this.baseUrl + this.imageSizes[2] + productionCompany.logo_path }
+		})
+
+		return productionCompanies
+	}
 }
