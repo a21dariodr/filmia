976429833f00d3d18c604eea63ff07cb273e2928
a21dariodr <a21dariodr@iessanclemente.net>
2023-11-08 08:37:24 +0100

    Cambios menores en el servicio de llamadas a la API TMDB

--- src/services/api/TheMovieDatabaseApiService.ts
+++ src/services/api/TheMovieDatabaseApiService.ts
@@ -5,8 +5,9 @@ import axios from 'axios'
 export default class TheMovieDatabaseApiService {
     readonly tmdApiAccessToken = import.meta.env.VITE_TMD_ACCESS_TOKEN
     readonly baseUrl = 'https://api.themoviedb.org/3/'
-    readonly imageBaseUrl = 'http://image.tmdb.org/t/p/'
+    readonly imageBaseUrl = 'https://image.tmdb.org/t/p/'
     readonly appendToResponse = '&append_to_response='
+    readonly imageSizes = ['w92/', 'w154/', 'w185/', 'w342/', 'w500/', 'w780/', 'original/']
 
     options = {
         method: 'GET',
@@ -15,7 +16,7 @@ export default class TheMovieDatabaseApiService {
             query: '',
             include_adult: 'true',
             language: 'es',
-            page: '1'
+            page: ''
         },
         headers: {
             accept: 'application/json',
@@ -23,16 +24,30 @@ export default class TheMovieDatabaseApiService {
         }
     }
 
-    public getMovieById() {
-        //TODO implementar método
-        if (!axios) console.log('Llamada a la API de TMD')
+    //TODO añadir procesamiento paracrear objeto film con la info y devolverlo
+	//TODO implementar funcion mapper?
+    public async getMovieById(id: string) {
+		let options = { ...this.options }
+		options.url = this.baseUrl + 'movie/' + id
+
+		const filmInfo = await axios.request(options)
+		const film = {}
+
+		return film
     }
 
-	//TODO añadir procesamiento para crear objetos Film con la info y devolverlos en un array
-    public searchMovieByTitle(titleQuery: string) {
+    //TODO añadir procesamiento para crear objetos Film con la info y devolverlos en un array
+    //TODO añadir url completa de la imagen al objeto
+	//TODO implementar función mapper?
+    public async searchMovieByTitle(titleQuery: string) {
         let options = { ...this.options }
-        options.url = this.baseUrl + '/search/movie'
-		options.params.query = titleQuery
-		return axios.request(options)
+        options.url = this.baseUrl + 'search/movie'
+        options.params.query = titleQuery
+		options.params.page = '1'
+
+        const searchResult = await axios.request(options)
+		const filmsFound: any = []
+
+		return filmsFound
     }
 }
