8825d9cb2e56e720e46b84b207870316a3553c05
a21dariodr <a21dariodr@iessanclemente.net>
2023-11-12 20:42:39 +0100

    Finalizado autocompletado para guardado de nuevas películas

--- src/layouts/NewFilmForm.tsx
+++ src/layouts/NewFilmForm.tsx
@@ -9,32 +9,37 @@ const NewFilmForm = () => {
     const { t } = useTranslation()
     const navigate = useNavigate()
 	const tmd = new TheMovieDatabaseApiService()
+	const [ searching, setSearching ] = useState<boolean>(false)
 	const [ titleSearch, setTitleSearch] = useState('')
 	const [ filmsFound, setFilmsFound ] = useState<Film[]>([])
 	const [ selectedFilm, setSelectedFilm ] = useState<Film | null>(null)
+	const [ watched, setWatched ] = useState<boolean>(false)
 
 	const searchByTitleHandler = async (event: React.ChangeEvent<HTMLInputElement>) => {
-		if (selectedFilm) setSelectedFilm(null)
 		const searchInputValue = event.target.value
-		console.log('Search input value: ', searchInputValue)
+		searchInputValue ? setSearching(true) : setSearching(false)
+		console.debug('Search input value: ', searchInputValue)
 		setTitleSearch(searchInputValue)
     }
 
-    const closeHandler = () => navigate(-1)
-
-    // TODO
     const selectFilmHandler = (film: Film) => {
-		console.log('Selected film: ', film)
-		const searchResultsDiv = document.querySelector('#searchResults')
-		searchResultsDiv?.classList.remove('flex')
-        searchResultsDiv?.classList.add('hidden')
+		console.debug('Selected film: ', film)
+		document.querySelector('#filmPoster')!.classList.remove('hidden')
+		setSearching(false)
+		setTitleSearch(film.title)
 		setSelectedFilm(film)
 	}
+
+	const saveFilm = () => {console.debug('ey')}
+
+	const closeHandler = () => navigate(-1)
+
+	const toggleWatched = () => setWatched(!watched)
 
 	useEffect( () => {
 		const searchResultsDiv = document.querySelector('#searchResults')
 
-		if (titleSearch) {
+		if (searching && titleSearch) {
 			tmd.searchMovieByTitle(titleSearch)
                 .then(films => {
                     if (films.length > 0) {
@@ -44,7 +49,7 @@ const NewFilmForm = () => {
                         searchResultsDiv?.classList.remove('flex')
                         searchResultsDiv?.classList.add('hidden')
                     }
-					console.log('Films found: ', films)
+					console.debug('Films found: ', films)
                     setFilmsFound([...films])
                 })
                 .catch( error => console.debug(error) )           
@@ -64,38 +69,126 @@ const NewFilmForm = () => {
                         <div className="flex flex-wrap justify-center px-4 md-px-0">
                             <div className="flex flex-wrap w-full md:w-1/2">
                                 <label htmlFor="title" className="mb-2 text-sm text-start text-gray-900">
-                                    {t('new_film.film_title')}*
+                                    {t('film.film_title')}*
                                 </label>
                                 <input
                                     id="title"
                                     type="text"
-                                    placeholder={t('new_film.film_title')}
-                                    value={selectedFilm ? selectedFilm.title : titleSearch}
+                                    placeholder={t('film.film_title')}
+                                    value={titleSearch}
                                     onChange={searchByTitleHandler}
-                                    className="flex items-center w-full px-5 py-4 mr-2 text-sm font-medium outline-none focus:bg-gray-200 mb-5 placeholder:text-gray-700 bg-gray-100 text-dark-gray-900 rounded-2xl"
+                                    className="flex items-center w-full px-5 py-4 mr-2 text-sm font-medium outline-none hover:bg-indigo-200 mb-5 placeholder:text-gray-700 bg-indigo-100 text-dark-gray-900 rounded-2xl"
                                 />
                             </div>
+                            <div className="flex flex-wrap w-full md:w-1/2 gap-4">
+                                <label className="mb-2 text-sm text-start text-gray-900">
+                                    {t('film.film_poster')}
+                                    <span className="material-symbols-outlined text-sm ml-1">lock</span>
+                                </label>
+                                <div id="filmPoster" className="mb-2 md:mb-0 hidden">
+                                    {selectedFilm?.posterPath ? (
+                                        <img src={selectedFilm.posterPath} className="w-20 h-20 object-cover" />
+                                    ) : (
+                                        <div className="flex flex-wrap place-content-center w-20 h-20 border">
+                                            <span className="material-symbols-outlined text-indigo-600 text-2xl">image</span>
+                                            <p className="text-sm md:text-md font-bold text-indigo-600">{t('film.image_error')}</p>
+                                        </div>
+                                    )}
+                                </div>
+                            </div>
+
                             <div className="flex flex-wrap w-full md:w-1/2">
                                 <label htmlFor="originalTitle" className="mb-2 text-sm text-start text-gray-900">
-                                    {t('new_film.film_original_title')}*
+                                    {t('film.film_original_title')}
+                                    <span className="material-symbols-outlined text-sm ml-1">lock</span>
                                 </label>
                                 <input
                                     id="originalTitle"
                                     type="text"
                                     readOnly
-                                    placeholder={t('new_film.film_original_title')}
+                                    placeholder={t('film.film_original_title')}
                                     value={selectedFilm ? selectedFilm.originalTitle : ''}
-                                    className="flex items-center w-full px-5 py-4 mb-5 mr-2 text-sm font-medium outline-none focus:bg-gray-200 placeholder:text-gray-700 bg-gray-100 text-dark-gray-900 rounded-2xl"
+                                    className="flex items-center w-full px-5 py-4 mb-5 mr-2 text-sm font-medium outline-none placeholder:text-gray-700 bg-gray-100 text-dark-gray-900 rounded-2xl"
+                                />
+                            </div>
+                            <div className="flex flex-wrap w-full md:w-1/2">
+                                <label htmlFor="releaseYear" className="mb-2 text-sm text-start text-gray-900">
+                                    {t('film.film_release_year')}
+                                    <span className="material-symbols-outlined text-sm ml-1">lock</span>
+                                </label>
+                                <input
+                                    id="releaseYear"
+                                    type="text"
+                                    readOnly
+                                    placeholder={t('film.film_release_year')}
+                                    value={selectedFilm ? selectedFilm.releaseYear : ''}
+                                    className="flex items-center w-full px-5 py-4 mb-5 mr-2 text-sm font-medium outline-none placeholder:text-gray-700 bg-gray-100 text-dark-gray-900 rounded-2xl"
+                                />
+                            </div>
+
+                            <div className="flex flex-wrap w-full md:w-1/2">
+                                <label htmlFor="score" className="mb-2 text-sm text-start text-gray-900">
+                                    {t('film.film_score')}
+                                </label>
+                                <input
+                                    id="score"
+                                    type="number"
+                                    min="0"
+                                    max="10"
+                                    step="0.01"
+                                    placeholder={t('film.film_score_placeholder')}
+                                    className="flex items-center w-full px-5 py-4 mb-5 mr-2 text-sm font-medium outline-none hover:bg-indigo-200 placeholder:text-gray-700 bg-indigo-100 text-dark-gray-900 rounded-2xl"
+                                />
+                            </div>
+                            <div className="flex flex-wrap w-full md:w-1/2">
+                                <label htmlFor="voteAverage" className="mb-2 text-sm text-start text-gray-900">
+                                    {t('film.film_vote_average')}
+                                    <span className="material-symbols-outlined text-sm ml-1">lock</span>
+                                </label>
+                                <input
+                                    id="voteAverage"
+                                    type="text"
+                                    readOnly
+                                    placeholder={t('film.film_vote_average')}
+                                    value={selectedFilm ? Math.round(selectedFilm.voteAverage * 100) / 100 : ''}
+                                    className="flex items-center w-full px-5 py-4 mb-5 mr-2 text-sm font-medium outline-none placeholder:text-gray-700 bg-gray-100 text-dark-gray-900 rounded-2xl"
+                                />
+                            </div>
+
+                            <div className="flex flex-wrap w-full md:w-1/2">
+                                <label className="text-sm text-gray-900">{t('film.film_watched')}</label>
+                                <div className="w-full mt-3 mb-5 py-4 px-5 mr-2 hover:bg-indigo-200 placeholder:text-gray-700 bg-indigo-100 text-dark-gray-900 rounded-2xl">
+                                    <label className="flex mr-3 cursor-pointer select-none">
+                                        <input type="checkbox" checked={watched} onChange={toggleWatched} className="sr-only peer" />
+                                        <div className="w-5 h-5 bg-white border-2 rounded-sm border-gray-500 peer peer-checked:border-0 peer-checked:bg-violet-700">
+                                            <img className="" src="check-icon.png" alt="tick" />
+                                        </div>
+                                        <span className="ml-3 text-sm font-medium text-gray-900">{t('film.film_watched')}</span>
+                                    </label>
+                                </div>
+                            </div>
+                            <div className="flex flex-wrap w-full md:w-1/2">
+                                <label htmlFor="id" className="mb-2 text-sm text-start text-gray-900">
+                                    {t('film.film_id')}
+                                    <span className="material-symbols-outlined text-sm ml-1">lock</span>
+                                </label>
+                                <input
+                                    id="id"
+                                    type="text"
+                                    readOnly
+                                    placeholder={t('film.film_id')}
+                                    value={selectedFilm ? selectedFilm.id : ''}
+                                    className="flex items-center w-full px-5 py-4 mb-5 mr-2 text-sm font-medium outline-none placeholder:text-gray-700 bg-gray-100 text-dark-gray-900 rounded-2xl"
                                 />
                             </div>
                         </div>
 
                         <div className="text-sm leading-relaxed my-3 text-gray-900">
                             <Button onClick={closeHandler} size="lg" className="text-xs text-dark-gray-900 font-bold bg-gray-300 hover:bg-gray-200 mr-2">
-                                {t('common.come_back')}
+                                {t('common.cancel')}
                             </Button>
 
-                            <Button size="lg" className="text-xs bg-violet-700 hover:bg-violet-600 ml-1">
+                            <Button onClick={saveFilm} size="lg" className="text-xs bg-violet-700 hover:bg-violet-600 ml-1">
                                 {t('new_film.save_film')}
                             </Button>
                         </div>
@@ -110,7 +203,7 @@ const NewFilmForm = () => {
                                         ) : (
                                             <div className="flex flex-wrap place-content-center w-20 h-20 border">
                                                 <span className="material-symbols-outlined text-indigo-600 text-2xl">image</span>
-                                                <p className="text-sm md:text-md font-bold text-indigo-600">{t('new_film.image_error')}</p>
+                                                <p className="text-sm md:text-md font-bold text-indigo-600">{t('film.image_error')}</p>
                                             </div>
                                         )}
                                         <div className="w-[70%] text-start">{film.title}</div>
