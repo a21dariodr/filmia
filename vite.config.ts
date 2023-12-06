422f95d761f878b5f3351c61bdc1f6eaf4d68d45
a21dariodr <a21dariodr@iessanclemente.net>
2023-12-06 12:46:04 +0100

    Suprimidos logs en consola al construir la aplicación

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,21 @@
 import { defineConfig, splitVendorChunkPlugin, type PluginOption } from 'vite'
 import react from '@vitejs/plugin-react'
+import terser from '@rollup/plugin-terser'
 import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-		react(),
-		splitVendorChunkPlugin(),
-		visualizer() as PluginOption
-	],
+    plugins: [react(), splitVendorChunkPlugin(), visualizer() as unknown as PluginOption],
     build: {
         rollupOptions: {
-			output: {
-				manualChunks(id: string) { }	
-			}
-		}
+            output: {
+                manualChunks(id: string) {}
+            },
+            plugins: [
+                terser({
+                    compress: { drop_console: true }
+                }) as unknown as PluginOption
+            ]
+        }
     }
 })
